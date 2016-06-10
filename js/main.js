8762f90f19475b28b4ca04b13ba98edd6a55b9ae
meganmotis <meganmotis@gmail.com>
2016-06-10 19:37:48 -0400

    fourth swipe

--- js/main.js
+++ js/main.js
@@ -1,28 +1,15 @@
 
 $(document).on('ready', function() {
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+	window.onload = function() {
 
-	var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
-    	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    	subdomains: ['otile1','otile2','otile3','otile4']
-	});
+	var tileLayer = L.tileLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png", {
+		maxZoom: 18, 
+		attribution: 'MRLC, State of Oregon, State of Oregon DOT, State of Oregon GEO, Esri, DeLorme, HERE, TomTom, USGS, NGA, EPA, NPS, U.S. Forest Service'
+		});
 
-	var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-    	attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    	subdomains: ['otile1','otile2','otile3','otile4']
-	});
-
-	var mapLayers = {
-    	"Satellite": satLayer,
-    	"Map View": drawLayer,
-    	"Open Street Maps": osm
+		mymap = L.map('mapid', {
+		layers: tileLayer
+		}).setView([45.891, -123.961], 12);
+	
 	};
-
-	var map = L.map('map-container').setView([45.891, -123.961], 13);
-
-	L.control.layers(mapLayers).addTo(map);
-	satLayer.addTo(map);
-
 });
