930eeb9422e7e141a63de4dc032eb4a4ad57e5de
meganmotis <meganmotis@gmail.com>
2016-06-11 00:51:05 -0400

    finished assignment

--- js/main.js
+++ js/main.js
@@ -1,15 +1,39 @@
 
 $(document).on('ready', function() {
-	window.onload = function() {
 
 	var tileLayer = L.tileLayer("http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png", {
 		maxZoom: 18, 
-		attribution: 'MRLC, State of Oregon, State of Oregon DOT, State of Oregon GEO, Esri, DeLorme, HERE, TomTom, USGS, NGA, EPA, NPS, U.S. Forest Service'
-		});
-
-		mymap = L.map('mapid', {
+	});
+		
+	mymap = L.map('mapid', {
 		layers: tileLayer
 		}).setView([45.891, -123.961], 12);
 	
-	};
+	$('#contact-form').validate({
+		submitHandler: function(form) {
+			form.submit();
+		},
+	
+		rules: {
+			"name": {
+				required: true,
+				maxlength: 128,
+				lettersonly: true
+			},
+		
+			"email": {
+				required: true,
+				email: true,
+			},
+		
+			"telephone": {
+				digitsonly: true,
+			},
+		
+			"message": {
+				required: true,
+				minlength: 20
+			}
+		}
+	});
 });
